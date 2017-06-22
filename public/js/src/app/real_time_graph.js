16b7206c3b4ae1ec40dfc56ac6db74b3658c4d88
ojoven <mikeltorresmail@gmail.com>
2017-06-22 16:58:21 +0200

    Refactor: real time graph code to Vue component

--- public/js/src/app/real_time_graph.js
+++ public/js/src/app/real_time_graph.js
@@ -1,19 +1,48 @@
-var options = {maxValueScale:1.28, grid:{fillStyle:'#ffffff',strokeStyle:'#f0f0f0',sharpLines:true},labels:{fillStyle:'#0d0d0d', precision: 0}};
-var smoothie = new SmoothieChart(options);
-smoothie.streamTo(document.getElementById("canvas"));
+Vue.component('real-time-graph', {
+	template: `
+		<div id="real_time_graph">
+			<canvas id="canvas" width="400" height="100"></canvas>
+		</div>
+  `,
+	data() {
+		return {
+			initial_delay: 100,
+			num_tweets_graph: 0,
+			options: {},
+			graph: null,
+			line: null
+		}
+	},
+	created: function() {
 
-// Data
-var num_tweets_graph = 0;
-var line1 = new TimeSeries();
+		setTimeout(function() {
 
-// Add a random value to each line every second
-setInterval(function() {
-	line1.append(new Date().getTime(), num_tweets_graph);
-}, 1000);
+			var that = this;
 
-// Add to SmoothieChart
-smoothie.addTimeSeries(line1, {lineWidth:2,strokeStyle:'#CD2626'});
+			// Initialize graph and lines
+			that.line = new TimeSeries();
+			that.options = { maxValueScale: 1.28, grid: { fillStyle:'#ffffff', strokeStyle: '#f0f0f0', sharpLines: true } ,labels: {fillStyle: '#0d0d0d', precision: 0 } };
+			that.graph = new SmoothieChart(that.options);
+			that.graph.streamTo(document.getElementById("canvas"), 0);
 
-socket.on('number_tweets', function(data) {
-	num_tweets_graph = data.number_tweets;
-}.bind(this));
+			// Add a random value to each line every second
+			setInterval(function() {
+				that.line.append(new Date().getTime(), that.num_tweets_graph, false);
+			}, 1000);
+
+			// Add to line to graph
+			that.graph.addTimeSeries(that.line, { lineWidth:2, strokeStyle:'#CD2626' } );
+
+			// Update the number of tweets
+			socket.on('number_tweets', function(data) {
+				that.num_tweets_graph = data.number_tweets;
+			}.bind(this));
+
+		}, this.initial_delay);
+
+	},
+	methods: {
+		
+	}
+
+});
