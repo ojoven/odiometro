49c6aeebcc2f9bd3608cffb258907a4032dc32cc
ojoven <mikeltorresmail@gmail.com>
2020-05-29 18:01:04 +0200

    Spacing

--- app/lib/twitterStream.js
+++ app/lib/twitterStream.js
@@ -1,13 +1,20 @@
 /** TWITTER STREAM **/
-module.exports = function(twitter) {
+module.exports = function (twitter) {
 
 	var track = require(global.appRoot + '/public/track_' + global.lang + '.json');
 	var twitterStream;
 
 	if (global.lang === 'es') {
-		twitterStream = twitter.stream('statuses/filter', { track: track, language: 'es', place: 'Spain' });
+		twitterStream = twitter.stream('statuses/filter', {
+			track: track,
+			language: 'es',
+			place: 'Spain'
+		});
 	} else {
-		twitterStream = twitter.stream('statuses/filter', { track: track, language: 'en'});
+		twitterStream = twitter.stream('statuses/filter', {
+			track: track,
+			language: 'en'
+		});
 	}
 
 	return twitterStream;
